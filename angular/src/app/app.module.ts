c319405e4686045c084b83a25c09d33082e20dbc
wayiok <wacho_jax@hotmail.com>
2020-10-08 22:04:42 -0500

    Video de fondo

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { BackgroundVideoComponent } from './background-video/background-video.component';
+import { PricesComponent } from './prices/prices.component';
+import { HeaaderComponent } from './heaader/heaader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundVideoComponent,
+    PricesComponent,
+    HeaaderComponent
   ],
   imports: [
     BrowserModule,
